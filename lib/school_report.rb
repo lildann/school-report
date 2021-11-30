ed0493dfdb7dd52d9e7ed3cc25f6e292df10be0d
Lilly Daniell <daniell.lilly@gmail.com>
2021-11-30 11:26:42 +0000

    Test passes for two scores

--- lib/school_report.rb
+++ lib/school_report.rb
@@ -1,9 +1,17 @@
 class SchoolReport
   def add_scores(scores)
+
+    results = scores.split(", ")
     green_total = 0
-    if scores == "Green"
-      green_total += 1
+    amber_total = 0
+
+    results.each do |result| 
+      if result == "Green"
+        green_total += 1
+      elsif result == "Amber"
+        amber_total += 1
+      end
     end
-      "Test 1:\nGreen: #{green_total}\nAmber: 0\nRed: 0"
+      "Test 1:\nGreen: #{green_total}\nAmber: #{amber_total}\nRed: 0"
   end
 end
--- spec/school_report_spec.rb
+++ spec/school_report_spec.rb
@@ -5,4 +5,9 @@ describe SchoolReport do
     school_report = SchoolReport.new
     expect(school_report.add_scores("Green")).to eq("Test 1:\nGreen: 1\nAmber: 0\nRed: 0")
   end
+
+  it 'summarises test results for two test scores' do
+    school_report = SchoolReport.new
+    expect(school_report.add_scores("Green, Amber")).to eq("Test 1:\nGreen: 1\nAmber: 1\nRed: 0")
+  end
 end
