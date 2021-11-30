4a6c22295787f637ecb83f9597fc4d7a32ad5c60
Lilly Daniell <daniell.lilly@gmail.com>
2021-11-30 11:35:48 +0000

    Refactor to include #generate_report

--- lib/school_report.rb
+++ lib/school_report.rb
@@ -1,17 +1,29 @@
 class SchoolReport
-  def add_scores(scores)
+  attr_reader :green_total, :amber_total, :red_total
 
+  def initialize
+    @green_total = 0
+    @amber_total = 0
+    @red_total = 0
+  end
+  
+  def add_scores(scores)
     results = scores.split(", ")
-    green_total = 0
-    amber_total = 0
 
     results.each do |result| 
       if result == "Green"
-        green_total += 1
+        @green_total += 1
       elsif result == "Amber"
-        amber_total += 1
+        @amber_total += 1
+      elsif result == "Red"
+        @red_total += 1
       end
     end
-      "Test 1:\nGreen: #{green_total}\nAmber: #{amber_total}\nRed: 0"
+    generate_report
   end
+
+  def generate_report
+    "Test 1:\nGreen: #{@green_total}\nAmber: #{@amber_total}\nRed: #{@red_total}"
+  end
+  
 end
--- spec/school_report_spec.rb
+++ spec/school_report_spec.rb
@@ -1,13 +1,22 @@
 require 'school_report'
 
 describe SchoolReport do
+  let(:school_report) { SchoolReport.new }
+
   it 'summarises test results for one test score' do
-    school_report = SchoolReport.new
     expect(school_report.add_scores("Green")).to eq("Test 1:\nGreen: 1\nAmber: 0\nRed: 0")
   end
 
   it 'summarises test results for two test scores' do
-    school_report = SchoolReport.new
     expect(school_report.add_scores("Green, Amber")).to eq("Test 1:\nGreen: 1\nAmber: 1\nRed: 0")
+  end
+
+  it 'summarises test results for two three different scores' do
+    expect(school_report.add_scores("Green, Amber, Red")).to eq("Test 1:\nGreen: 1\nAmber: 1\nRed: 1")
+  end
+
+  it 'generates a report' do
+    school_report.add_scores("Green, Amber, Red")
+    expect(school_report.generate_report).to eq("Test 1:\nGreen: 1\nAmber: 1\nRed: 1")
   end
 end
